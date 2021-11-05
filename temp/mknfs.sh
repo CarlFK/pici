ed0a5c59ae58beb5005c1a22150417da547fa8b2
Carl Karsten <carl@personnelware.com>
2021-11-05 09:11:21 -0500

    move bilibop debuging examples into temp

--- temp/mknfs.sh
+++ temp/mknfs.sh
@@ -1,3 +1,9 @@
+# setup the server
+# nfs, host the root fs (boot is on local storage)
+
+apt install nfs-kernel-server
+# etc/exports:
+# /srv/nfs/x86/be *(ro,sync,no_subtree_check,no_root_squash,fsid=3)
 
 mkdir -p /srv/nfs/x86/be
 cd /srv/nfs/x86
@@ -5,4 +11,7 @@ debootstrap stable be
 chroot be passwd
 # bilibop-rules bilibop-rules/on-live-system boolean true
 chroot be apt install bilibop
+
+systemctl enable nfs-kernel-server
+systemctl restart nfs-kernel-server
 
