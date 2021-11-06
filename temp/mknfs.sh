47872e98327835928e9db96b1f6533588f53a737
Carl Karsten <carl@personnelware.com>
2021-11-06 01:30:18 -0500

    overlayfs and bilibop

--- temp/mknfs.sh
+++ temp/mknfs.sh
@@ -2,13 +2,26 @@
 # client is a debian or ubuntu box with grub and /boot/ to boot a kernel
 # kernel mounts nfs on /
 
-# negk=nfs server
-# gator: local boot, nfs client
+# negk: nfs server
+# gator: local boot, nfs client (debian w/sshd)
 
-# You will need to find these, put them in focal/
+# You will need to find these, put them here so they can be sent to client
 # I got them from a Ubuntu install.
 # vmlinuz-5.4.0-89-generic
 # initrd.img-5.4.0-89-generic
+rsync -rtvP 40_custom vmlinuz* initrd* juser@gator:
+# Config client to use it:
+ssh juser@gator
+# on gator:
+# put Ubuntu kernele in /boot
+sudo cp vmlinuz* initrd* /boot/
+# add debian and ubuntu on nfs to grub
+sudo cp 40_custom /etc/grub.d/;sudo update-grub;sudo reboot
+
+# At this poing the local install should still work,
+# the 2 new options should kernel panic.
+
+# Set up the server
 
 # on Server:
 apt install nfs-kernel-server
@@ -23,20 +36,11 @@ cd /srv/nfs/x86
 debootstrap stable bullseye
 debootstrap focal focal http://archive.ubuntu.com/ubuntu/
 
-# server done.  Config client to use it:
-rsync -rtvP 40_custom bulseye focal juser@gator:
-ssh juser@gator
-# on gator:
-# put Ubuntu kernele in /boot
-sudo cp focal/* /boot/
-# add debian and ubuntu on nfs to grub
-sudo cp 40_custom /etc/grub.d/;sudo update-grub;sudo reboot
-
+# server ready to test.
+# before you set the root passwords,
 # boot client, make sure the 2 local/nfs items don't kernel panic.
 
 # set root password and install things
-
-cd /srv/nfs/x86
 chroot bullseye passwd
 chroot focal passwd
 
@@ -45,10 +49,14 @@ chroot bullseye apt install bilibop
 # lockfs is already in place (see 40_custom)
 
 chroot focal apt install overlayroot
-cp overlayroot.conf /srv/nfs/x86/focal/etc
+cp ${OLDPWD}/overlayroot.conf /srv/nfs/x86/focal/etc
 
 # boot the clients - focal has a rw ram fs on top of rw nfs.
 # bullseye ... no change.
+
+exit
+
+# samples of what / is mounted on:
 
 # success!
 juser@negk:~$ findmnt /
