01692f0ca041c52db8c46c9d15ec032a029e61d0
Carl Karsten <carl@personnelware.com>
2021-11-09 02:38:09 -0600

    boot pi from sd, / on nfs

---
+++ temp/mkpisrv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ex
+
+# edit cmdline.txt, replace my IP with your IP, put it in your pi's boot.
+cat <<EOT > cmdline.txt
+root=/dev/nfs nfsroot=192.168.1.119:/srv/nfs/pi/root,vers=4.1 rw ip=dhcp rootwait elevator=deadline consoleblank=0 overlayroot=tmpfs,debug=1
+EOT
+ip r>> cmdline.txt
+
+apt install nfs-kernel-server unzip kpartx
+
+img_host=http://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-05-28
+base_name=2021-05-07-raspios-buster-armhf-lite
+zip_name=${base_name}.zip
+img_name=${base_name}.img
+
+export http_proxy=http://pc8:8000/
+wget -N ${img_host}/${zip_name}
+unzip -u ${zip_name}
+kpartx -av ${img_name}
+mkdir img2
+mount /dev/mapper/loop0p2 img2
+
+mkdir -p /srv/nfs/pi/root
+rsync -xa --progress img2/ /srv/nfs/pi/root
+umount img2
+
+wget -N http://launchpadlibrarian.net/493868580/overlayroot_0.47ubuntu1_all.deb
+cp overlayroot_0.47ubuntu1_all.deb /srv/nfs/pi/root/home/pi
+
+cat <<EOT >> /etc/exports
+/srv/nfs/pi/root *(rw,sync,no_subtree_check,no_root_squash)
+EOT
+systemctl restart nfs-kernel-server
+
