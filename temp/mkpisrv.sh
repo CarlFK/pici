7c0487ebeecf336a9b8b8ad99164a338aa37bb30
Carl Karsten <carl@personnelware.com>
2021-11-10 04:31:09 -0600

    raspi-debian nfs demo

--- temp/mkpisrv.sh
+++ temp/mkpisrv.sh
@@ -1,21 +1,28 @@
 #!/bin/bash -ex
 
 # edit cmdline.txt, replace my IP with your IP, put it in your pi's boot.
-cat <<EOT > cmdline.txt
+cat <<EOT >> cmdline.txt
 root=/dev/nfs nfsroot=192.168.1.119:/srv/nfs/pi/root,vers=4.1 rw ip=dhcp rootwait elevator=deadline consoleblank=0 overlayroot=tmpfs,debug=1
 EOT
 ip r>> cmdline.txt
 
-apt install nfs-kernel-server unzip kpartx
+apt install nfs-kernel-server unzip kpartx dcfldd
 
-img_host=http://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-05-28
-base_name=2021-05-07-raspios-buster-armhf-lite
-zip_name=${base_name}.zip
+img_host=http://raspi.debian.net/daily
+base_name=raspi_4_bullseye
+zip_name=${base_name}.img.xz
 img_name=${base_name}.img
+
+# img_host=http://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-05-28
+# base_name=2021-05-07-raspios-buster-armhf-lite
+# zip_name=${base_name}.zip
+# img_name=${base_name}.img
 
 export http_proxy=http://pc8:8000/
 wget -N ${img_host}/${zip_name}
-unzip -u ${zip_name}
+# unzip -u ${zip_name}
+xz --keep -vv --decompress ${zip_name}
+echo sudo dcfldd if=${img_name} of=/dev/sdb
 kpartx -av ${img_name}
 mkdir img2
 mount /dev/mapper/loop0p2 img2
