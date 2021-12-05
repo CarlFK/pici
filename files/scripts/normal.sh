3679334bfdab57ac4cf8dc36dae9f1f27327bbad
Carl Karsten <carl@personnelware.com>
2021-12-04 17:46:30 -0800

    don't auto mount the pi files, might brick the server

--- files/scripts/normal.sh
+++ files/scripts/normal.sh
@@ -9,18 +9,31 @@ p=/srv/nfs/rpi/${dist}
 boot=${p}/boot/merged
 root=${p}/root/merged
 
+umount ${p}/boot/merged || true
+umount ${p}/root/merged || true
+
+rm -rf ${p}/boot/work/index
+rm -rf ${p}/root/work/index
+
+mount -o rw ${p}/boot/merged
+mount -o rw ${p}/root/merged
+
 # turn on overlayroot
 sed -i "/.*/s/overlayroot=/overlayroot=tmpfs/" ${boot}/cmdline.txt
 
 # don't automount pi's /boot
 sed -i "/.boot nfs*/s/,auto,/,noauto,/" ${root}/etc/fstab
 
+# done with pi files, so ro all of them:
+mount -o remount,ro ${p}/boot/merged
+mount -o remount,ro ${p}/root/merged
+
 # make the nfs shares ro
 sed -i "/.*/s/rw,/ro,/" /etc/exports
 systemctl restart nfs-server.service
 
 # server enable automount the stack of pi files
-sed -i "\@overlay\s*${p}/[br]oot/merged@s@\bnoauto\b@auto@" /etc/fstab
-
-echo "reboot pi"
+# (this risks bricking the server with:
+#  overlayfs: failed to verify index dir 'upper' xattr
+# sed -i "\@overlay\s*${p}/[br]oot/merged@s@\bnoauto\b@auto@" /etc/fstab
 
