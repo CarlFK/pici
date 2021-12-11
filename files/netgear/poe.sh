46989df6084f85f52ea9be21a14d5b66221ac908
Carl Karsten <carl@personnelware.com>
2021-12-11 10:14:58 -0800

    poe.sh shows status of ports

--- files/netgear/poe.sh
+++ files/netgear/poe.sh
@@ -3,6 +3,8 @@
 # poe.sh port 1|2
 # 1=on, 2=off
 
+# no parameters shows status of 26,30,36 (ports currently in use)
+
 # Netgear S3300-52X-PoE+
 # Sytem, SNMP, Community Configuration,
 # Management Station IP: 10.21.0.1 (server's eth-local IP)
@@ -14,4 +16,10 @@
 
 # sudo apt intall snmp
 
-snmpset -v1 -c pib 10.21.0.171 iso.3.6.1.2.1.105.1.1.1.3.1.$1 i $2
+if [ $# -eq 0 ]; then
+    for port in 26 30 36; do
+        snmpget -v1 -c pib 10.21.0.171 iso.3.6.1.2.1.105.1.1.1.3.1.$port
+    done
+else
+    snmpset -v1 -c pib 10.21.0.171 iso.3.6.1.2.1.105.1.1.1.3.1.$1 i $2
+fi
