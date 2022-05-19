7ca73ea5b3f774ffee34672d91310bce54408b12
Carl Karsten <carl@personnelware.com>
2022-05-18 19:03:23 -0500

    move xz--decompress into extract script

--- ansible/roles/img/files/img2files.sh
+++ ansible/roles/img/files/img2files.sh
@@ -3,8 +3,12 @@
 
 set -ex
 
+zip_name=/var/cache/pib/2022-04-04-raspios-bullseye-armhf-lite.img.xz
 img_name=/var/cache/pib/2022-04-04-raspios-bullseye-armhf-lite.img
 dist=bullseye
+
+cd /var/cache/pib/
+xz --keep --decompress ${zip_name} | true
 
 # trunk of nfs things, destination for files
 nfs_dir=/srv/nfs/rpi/${dist}
@@ -16,13 +20,13 @@ partprobe
 mkdir -p /tmp/img
 
 mount /dev/loop5p1 /tmp/img
-mkdir ${nfs_dir}/boot
-rsync -xa --progress /tmp/img/ boot
+mkdir -p ${nfs_dir}/boot
+rsync -xa --progress /tmp/img/ ${nfs_dir}/boot
 umount /tmp/img
 
 mount /dev/loop5p2 /tmp/img
-mkdir ${nfs_dir}/root
-rsync -xa --progress /tmp/img/ root
+mkdir -p ${nfs_dir}/root
+rsync -xa --progress /tmp/img/ ${nfs_dir}/root
 umount /tmp/img
 
 losetup -d /dev/loop5
