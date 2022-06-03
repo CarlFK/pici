7fbc86bb739601192de6c632df6dc6e1a9607ccd
Carl Karsten <carl@personnelware.com>
2022-06-02 23:30:49 -0500

    pw without pipe

--- ansible/roles/fixpi/files/pipw.sh
+++ ansible/roles/fixpi/files/pipw.sh
@@ -16,10 +16,15 @@ printf "%s\n" ${pass} >>  root/etc/issue
 
 # pre-seed raspios's make a new users
 # echo 'mypassword' | openssl passwd -6 -stdin
-crypt_pas=$(openssl passwd -6 env:pass)
+# crypt_pas=$(echo 'mypas' | openssl passwd -6 -stdin)
+# crypt_pas=$(echo "$pass" | openssl passwd -6 -stdin)
+# crypt_pas=$(openssl passwd -6 env:pass)
+# crypt_pas=$(openssl passwd -6 -file:root/etc/ssh/password.txt)
+crypt_pas=$(openssl passwd -6 -in root/etc/ssh/password.txt)
 printf "%s:%s" ${user} ${crypt_pas} > boot/userconf.txt
 
 # this sets the password of an existing user
+# from back when raspios came with a user
 # crypt_pass=$(mkpasswd ${pass})
 # usermod --root $PWD/root/ --password ${crypt_pass} ${user}
 
