97bfc2a644ac37aade969e09c25a00b784730b6d
Carl Karsten <carl@personnelware.com>
2022-06-22 15:15:19 -0500

    remove audiotestsrc (so no beep) and use short hostname.

--- ansible/roles/cam/pi/files/gst-libcam.sh
+++ ansible/roles/cam/pi/files/gst-libcam.sh
@@ -6,12 +6,10 @@ ip=$(ip -json route show default | jq ".[0].gateway" --raw-output)
 echo ${ip}
 
 # RTMP_DEST=rtmp://${ip}/stream/${HOSTNAME}
-RTMP_DEST=rtmp://${ip}/stream/$(/usr/bin/hostname)
+RTMP_DEST=rtmp://${ip}/stream/$(/usr/bin/hostname --short)
 
 /usr/bin/gst-launch-1.0 libcamerasrc ! \
     videoconvert ! x264enc bitrate=1000 tune=zerolatency ! video/x-h264 ! h264parse ! \
     video/x-h264 ! queue ! flvmux name=mux ! \
-    rtmpsink location=$RTMP_DEST audiotestsrc is-live=true ! \
-    audioconvert ! audioresample ! audio/x-raw,rate=48000 ! \
-    voaacenc bitrate=96000 ! audio/mpeg ! aacparse ! audio/mpeg, mpegversion=4 ! mux.
+    rtmpsink location=$RTMP_DEST
 
