0c5380196f2d584d305a3f84e6a3416f5a67ae38
Carl Karsten <carl@personnelware.com>
2022-06-21 21:30:11 -0500

    it streams, with some help...

--- ansible/roles/cam/pi/files/gst-libcam.sh
+++ ansible/roles/cam/pi/files/gst-libcam.sh
@@ -7,7 +7,8 @@ set -ex
 ip=$(ip -json route show default | jq ".[0].gateway" --raw-output)
 echo ${ip}
 
-RTMP_DEST=rtmp://${ip}/stream/${HOSTNAME}
+# RTMP_DEST=rtmp://${ip}/stream/${HOSTNAME}
+RTMP_DEST=rtmp://${ip}/stream/$(hostname)
 
 gst-launch-1.0 libcamerasrc ! \
     videoconvert ! x264enc bitrate=1000 tune=zerolatency ! video/x-h264 ! h264parse ! \
