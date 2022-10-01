345a1d2e1da862a550f57d9e21c3e754e635098e
Carl Karsten <carl@personnelware.com>
2022-10-01 16:25:45 -0500

    use pi hostname for stream name

--- ansible/roles/cam/pi/files/gst-libcam.sh
+++ ansible/roles/cam/pi/files/gst-libcam.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -x
 
 # find the upstream IP
-# ip=$(ip -json route show default | jq ".[0].prefsrc" --raw-output)
 ip=$(ip -json route show default | jq ".[0].gateway" --raw-output)
-echo ${ip}
-
-# RTMP_DEST=rtmp://${ip}/stream/${HOSTNAME}
 RTMP_DEST=rtmp://${ip}/stream/$(/usr/bin/hostname --short)
 
 /usr/bin/gst-launch-1.0 libcamerasrc ! \
