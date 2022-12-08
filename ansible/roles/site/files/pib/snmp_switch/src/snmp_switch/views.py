abfa8c3d93d9ebc302451500eb96698f0e4e7b9e
Carl Karsten <carl@personnelware.com>
2022-12-07 22:18:46 -0600

    add the snmp_switch dir I'm trying to package

---
+++ ansible/roles/site/files/pib/snmp_switch/src/snmp_switch/views.py
@@ -0,0 +1,63 @@
+from django.conf import settings
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+from pprint import pprint
+
+from snmp_switch.utils import snmp_status, snmp_toggle
+
+# TODO: move settings into app settings/config.
+
+
+@csrf_exempt
+def toggle(request, extra_context={}):
+
+    port = request.POST['port']
+
+    context = {}
+    context.update(extra_context)
+
+    context.update(snmp_toggle(
+        host=settings.SNMP_SWITCH_HOST,
+        username=settings.SNMP_SWITCH_USERNAME,
+        authkey=settings.SNMP_SWITCH_AUTHKEY,
+        privkey=settings.SNMP_SWITCH_PRIVKEY,
+        oid=settings.SNMP_SWITCH_OID,
+        port=port))
+
+    pprint(context)
+
+    response = HttpResponse(content_type="application/json")
+    d={'port': port, 'value': 1}
+    json.dump(d, response, indent=2)
+
+    return response
+
+@csrf_exempt
+def status(request, template_name='snmp_status.html', extra_context={}):
+
+    port = request.POST['port']
+
+    context = {}
+    context.update(extra_context)
+
+    context.update(snmp_status(
+        host=settings.SNMP_SWITCH_HOST,
+        username=settings.SNMP_SWITCH_USERNAME,
+        authkey=settings.SNMP_SWITCH_AUTHKEY,
+        privkey=settings.SNMP_SWITCH_PRIVKEY,
+        oid=settings.SNMP_SWITCH_OID,
+        port=port))
+
+    pprint(context)
+
+    response = HttpResponse(content_type="application/json")
+    d={'port':port, 'value':1}
+    # json.dump(context, response, indent=2)
+    json.dump(d, response, indent=2)
+
+    return response
+    # return render(request, template_name=template_name, context=context)
