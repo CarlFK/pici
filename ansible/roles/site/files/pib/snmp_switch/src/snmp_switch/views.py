7c539ffa01daca21d9df8b731c877b7a52ad305a
Carl Karsten <carl@personnelware.com>
2023-01-27 16:30:58 -0800

    support more auth

--- ansible/roles/site/files/pib/snmp_switch/src/snmp_switch/views.py
+++ ansible/roles/site/files/pib/snmp_switch/src/snmp_switch/views.py
@@ -7,7 +7,20 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from pprint import pprint
 
+from pysnmp.hlapi import usmHMAC384SHA512AuthProtocol, usmNoPrivProtocol
 from snmp_switch.utils import snmp_set_state, snmp_status, snmp_toggle
+
+params = {
+        'host':settings.SNMP_SWITCH_HOST,
+        'username':settings.SNMP_SWITCH_USERNAME,
+        'authkey':settings.SNMP_SWITCH_AUTHKEY,
+        'privkey':settings.SNMP_SWITCH_PRIVKEY,
+        'oid':settings.SNMP_SWITCH_OID,
+        'port': None,
+        'authProtocol': usmHMAC384SHA512AuthProtocol,
+        'privProtocol': usmNoPrivProtocol,
+        }
+
 
 @csrf_exempt
 def toggle(request, extra_context={}):
@@ -17,19 +30,19 @@ def toggle(request, extra_context={}):
     context = {}
     context.update(extra_context)
 
-    context.update(snmp_toggle(
-        host=settings.SNMP_SWITCH_HOST,
-        username=settings.SNMP_SWITCH_USERNAME,
-        authkey=settings.SNMP_SWITCH_AUTHKEY,
-        privkey=settings.SNMP_SWITCH_PRIVKEY,
-        oid=settings.SNMP_SWITCH_OID,
-        port=port))
+    params['port'] = port
+    context.update(snmp_toggle( **params ))
 
     pprint(context)
 
     response = HttpResponse(content_type="application/json")
-    d={'port': port, 'value': 1}
+    d={     'port':port,
+            'value': None,
+             # 'params': params,
+            'context': context.__str__()
+        }
     json.dump(d, response, indent=2)
+
 
     return response
 
@@ -89,19 +102,16 @@ def status(request):
     port = request.POST['port']
 
     context = {}
-    context.update(snmp_status(
-        host=settings.SNMP_SWITCH_HOST,
-        username=settings.SNMP_SWITCH_USERNAME,
-        authkey=settings.SNMP_SWITCH_AUTHKEY,
-        privkey=settings.SNMP_SWITCH_PRIVKEY,
-        oid=settings.SNMP_SWITCH_OID,
-        port=port))
+
+    params['port'] = port
+    context.update(snmp_status( **params ))
 
     pprint(context)
 
     response = HttpResponse(content_type="application/json")
     d={     'port':port,
             'value': None,
+             # 'params': params,
             'context': context.__str__()
         }
     json.dump(d, response, indent=2)
