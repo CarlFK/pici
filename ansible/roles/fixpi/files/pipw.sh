40764492b4de1ccf92963d820bca74665bfb70ea
Carl Karsten <carl@personnelware.com>
2023-11-29 19:40:09 -0600

    fix fixpi to work when called from ansible.

--- ansible/roles/fixpi/files/pipw.sh
+++ ansible/roles/fixpi/files/pipw.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # generate a random password for pi user
-
-# pass in the username
+# usage: pipw.sh username [password]
 
 user=${1:-pi}
 
@@ -13,14 +12,17 @@ else
 fi
 
 # save it in the clear so everyone can see it when they want to root the box
-printf "%s\n" ${pass} > root/etc/ssh/password.txt
+printf "%s\n" ${pass} > etc/ssh/password.txt
 
 # append it to the text that is displayed on the console
-printf "%s\n" ${pass} >>  root/etc/issue
+printf "%s\n" ${pass} >>  etc/issue
 
-# pre-seed raspios's make a new users
-crypt_pas=$(openssl passwd -6 -in root/etc/ssh/password.txt)
-printf "%s:%s" ${user} ${crypt_pas} > boot/userconf.txt
+# pre-seed raspios's "make a new user" process
+crypt_pas=$(openssl passwd -6 -in etc/ssh/password.txt)
+# this works for fixit.sh, needs to be fixed for ansible.
+# fname=boot/firmware/userconf.txt
+fname=boot/userconf.txt
+printf "%s:%s" ${user} ${crypt_pas} > ${fname}
 
 # this sets the password of an existing user
 # from back when raspios came with a user
