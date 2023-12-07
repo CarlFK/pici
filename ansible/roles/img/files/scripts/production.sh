95b2b831f227da0a73c3a44923afa2caa89cff21
Carl Karsten <carl@personnelware.com>
2023-12-06 21:39:20 -0600

    use fancy sed to tweek fstab.

--- ansible/roles/img/files/scripts/production.sh
+++ ansible/roles/img/files/scripts/production.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -ex
 
-# normal.sh
-# normal / production mode.
+# production.sh
+# production mode.
 # use to get out of maintenance mode.
 
 # things it does:
 # nfs exports set to ro,
 # pi overlay tmpfs,
 # pi fstab: don't mount boot
-# pi fstab: don't mount boot
+# pi fstab: mount / ro
 
 dist=bookworm
 p=/srv/nfs/rpi/${dist}
@@ -25,7 +25,14 @@ sed -i -E "/.*/s/overlayroot=(tmpfs)?/overlayroot=tmpfs/" ${boot}/cmdline.txt
 cat ${boot}/cmdline.txt
 
 # don't automount pi's /boot
-sed -i "/.boot.* nfs*/s/,auto,/,noauto,/" ${root}/etc/fstab
+# sed -i "/.boot.* nfs*/s/,auto,/,noauto,/" ${root}/etc/fstab
+# pi fstab: mount / ro
+# um.. where is it?
+
+# take 2: use the sed from maintenance.sh...
+# pi's fstab: (no sd) set nfs mounts / and /boot to noauto(mount),ro
+sed -i "\@${p}.*\snfs\s@s@\bauto\b@noauto@" ${root}/etc/fstab
+sed -i "\@${p}.*\snfs\s@s@\brw\b@ro@" ${root}/etc/fstab
 
 # done with pi files, so ro all of them:
 # make the nfs shares ro
