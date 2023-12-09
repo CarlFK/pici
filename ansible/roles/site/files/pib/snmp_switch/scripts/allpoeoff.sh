c23ee02300d979f6f620ed7d136f4b3f9b365d3a
Carl Karsten <carl@personnelware.com>
2023-12-09 15:59:19 -0600

    overhaul snmp parameter handeling.

--- ansible/roles/site/files/pib/snmp_switch/scripts/allpoeoff.sh
+++ ansible/roles/site/files/pib/snmp_switch/scripts/allpoeoff.sh
@@ -2,18 +2,29 @@
 
 set -x
 
-sleep_time=${1:-0}
+sleep_time=${1:-10}
 
 pi_ports=$(seq 1 48)
 ip_base=10.21.0
 o_base=100
 
+# issue shutdown command to all the pis:
+# no wait, this is poiteless.
+# they are all overlayrooted, so the fs is in ram.
 for p in ${pi_ports}
 do
    let o=o_base+p
    pi_ip=${ip_base}.${o}
-   echo "ssh pi@${pi_ip} \"sudo shutdown\""
-   ssh pi@${pi_ip} "sudo shutdown"
-   # poe.sh $p $1
+   # ssh pi@${pi_ip} "sudo shutdown"
    # sleep ${sleep_time}
+done
+
+# sleep ${sleep_time}
+
+# drop power all the ports
+for p in ${pi_ports}
+do
+   let o=o_base+p
+   pi_ip=${ip_base}.${o}
+   poe.sh $p 2
 done
--- ansible/roles/site/files/pib/snmp_switch/src/snmp_switch/views.py
+++ ansible/roles/site/files/pib/snmp_switch/src/snmp_switch/views.py
@@ -7,42 +7,91 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from pprint import pprint
 
-from pysnmp.hlapi import usmHMAC384SHA512AuthProtocol, usmNoPrivProtocol
+from pysnmp import hlapi # import usmHMAC384SHA512AuthProtocol, usmNoPrivProtocol
 from snmp_switch.utils import snmp_set_state, snmp_status, snmp_toggle
 
+# construct params: a dictionary of parameters
+# the source of truth is the swich config page, stored as strings in ansible inventory.
+# some of the parameters are python objects.
+# so convert strings to python objects.
+
+"""
+   ansible inventory will use the strings used by snmpget.py:
+   -a AUTH-PROTOCOL      authentication protocol ID (MD5|SHA|SHA224|SHA256|SHA384|SHA512)
+
+   map those strings to pysnmp.hlapi constants:
+
+    usmNoAuthProtocol (default is authKey not given)
+    usmHMACMD5AuthProtocol (default if authKey is given)
+    usmHMACSHAAuthProtocol
+    usmHMAC128SHA224AuthProtocol
+    usmHMAC192SHA256AuthProtocol
+    usmHMAC256SHA384AuthProtocol
+    usmHMAC384SHA512AuthProtocol
+"""
+
+authProtocol={
+        'NoAuth': hlapi.usmNoAuthProtocol,
+        'MD5': hlapi.usmHMACMD5AuthProtocol,
+        'SHA': hlapi.usmHMACSHAAuthProtocol,
+        'SHA224': hlapi.usmHMAC128SHA224AuthProtocol,
+        'SHA256': hlapi.usmHMAC192SHA256AuthProtocol,
+        'SHA384': hlapi.usmHMAC256SHA384AuthProtocol,
+        'SHA512': hlapi.usmHMAC384SHA512AuthProtocol,
+        }[settings.SNMP_SWITCH_AUTH_PROTOCOL]
+
+"""
+same for
+    snmpget.py
+   -x PRIV-PROTOCOL      privacy protocol ID (3DES|AES|AES128|AES192|AES192BLMT|AES256|AES256BLMT|DES)
+
+    usmNoPrivProtocol (default is privhKey not given)
+    usmDESPrivProtocol (default if privKey is given)
+    usm3DESEDEPrivProtocol
+    usmAesCfb128Protocol
+    usmAesCfb192Protocol
+    usmAesCfb256Protocol
+"""
+
+privProtocol = {
+        'NoPriv': hlapi.usmNoPrivProtocol,
+        '3DES': hlapi.usm3DESEDEPrivProtocol,
+        'AES': None,
+        'AES128': hlapi.usmAesCfb128Protocol,
+        'AES192': hlapi.usmAesCfb192Protocol,
+        'AES192BLMT': None,
+        'AES256': hlapi.usmAesCfb256Protocol,
+        'AES256BLMT': None,
+        'DES': hlapi.usmDESPrivProtocol,
+        }[settings.SNMP_SWITCH_PRIV_PROTOCOL]
+
+
+# port is the rj45 port on the 24 or 48 or howmanyever port switch.
+# not tcp/udp port.  (which seems to be udp 161)
 params = {
         'host':settings.SNMP_SWITCH_HOST,
         'username':settings.SNMP_SWITCH_USERNAME,
         'authkey':settings.SNMP_SWITCH_AUTHKEY,
+        'authProtocol': authProtocol,
         'privkey':settings.SNMP_SWITCH_PRIVKEY,
+        'privProtocol': privProtocol,
         'oid':settings.SNMP_SWITCH_OID,
         'port': None,
-        'authProtocol': usmHMAC384SHA512AuthProtocol,
-        'privProtocol': usmNoPrivProtocol,
         }
 
+def all_to_str(o):
+    return o.__str__()
 
 @csrf_exempt
-def toggle(request, extra_context={}):
+def toggle(request):
+    # turn the port off and on again
 
-    port = request.POST['port']
-
-    context = {}
-    context.update(extra_context)
-
-    params['port'] = port
-    context.update(snmp_toggle( **params ))
-
-    pprint(context)
+    params['port'] = request.POST['port']
+    d=snmp_toggle( **params )
 
     response = HttpResponse(content_type="application/json")
-    d={     'port':port,
-            'value': None,
-             # 'params': params,
-            'context': context.__str__()
-        }
-    json.dump(d, response, indent=2)
-
+    s = d.__str__()
+    json.dump(s, response, indent=2)
 
     return response
 
@@ -52,16 +101,10 @@ def toggle_all(request):
     l = []
     for port in range(1,48):
 
-        d = snmp_toggle(
-            host=settings.SNMP_SWITCH_HOST,
-            username=settings.SNMP_SWITCH_USERNAME,
-            authkey=settings.SNMP_SWITCH_AUTHKEY,
-            privkey=settings.SNMP_SWITCH_PRIVKEY,
-            oid=settings.SNMP_SWITCH_OID,
-            port=str(port))
-
-        d={'port': port, 'value': 1}
-        l.append(d)
+        params['port'] = str(port)
+        d=snmp_toggle( **params )
+        s = d.__str__()
+        l.append(s)
 
     response = HttpResponse(content_type="application/json")
     json.dump(l, response, indent=2)
@@ -72,22 +115,15 @@ def toggle_all(request):
 def off_all(request):
 
     # 2=off
-    state=2
+    d['state']=2
 
     l = []
     for port in range(1,48):
 
-        d = snmp_set_state(
-            host=settings.SNMP_SWITCH_HOST,
-            username=settings.SNMP_SWITCH_USERNAME,
-            authkey=settings.SNMP_SWITCH_AUTHKEY,
-            privkey=settings.SNMP_SWITCH_PRIVKEY,
-            oid=settings.SNMP_SWITCH_OID,
-            port=str(port),
-            state=state)
-
-        d={'port': port, 'value': state}
-        l.append(d)
+        params['port'] = str(port)
+        d = snmp_set_state( **params )
+        s = d.__str__()
+        l.append(s)
 
     response = HttpResponse(content_type="application/json")
     json.dump(l, response, indent=2)
@@ -95,25 +131,17 @@ def off_all(request):
     return response
 
 
-
 @csrf_exempt
 def status(request):
 
-    port = request.POST['port']
+    params['port'] = request.POST['port']
+    d=snmp_status( **params )
 
-    context = {}
-
-    params['port'] = port
-    context.update(snmp_status( **params ))
-
-    pprint(context)
-
+    d = {'snmp_status': d, 'raw': d.__str__() }
     response = HttpResponse(content_type="application/json")
-    d={     'port':port,
-            'value': None,
-             # 'params': params,
-            'context': context.__str__()
-        }
-    json.dump(d, response, indent=2)
+    json.dump(d, response, indent=2, default=all_to_str)
+    s = d.__str__()
+    # json.dump(s, response, indent=2)
+    # json.dump(params, response, indent=2)
 
     return response
