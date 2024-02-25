0e58239836e8b6b93786264980b488e4e617c41c
Carl Karsten <carl@personnelware.com>
2024-02-25 15:49:45 -0600

    ajaxify Reset button

--- ansible/roles/site/files/pib/pistat/src/pistat/static/dcws.js
+++ ansible/roles/site/files/pib/pistat/src/pistat/static/dcws.js
@@ -7,33 +7,53 @@ function PiStatus(PiID) {
         'wss://'
         + window.location.host
         + '/ws/pistat/'
-        + 'pi'+PiID.toString()
+        + 'pi'+PiID
         + '/'
     );
 
     logSocket.onmessage = function(e) {
         const data = JSON.parse(e.data);
-        document.querySelector('#log'+PiID.toString()).value += (data.message + '\n');
+        document.querySelector('#log'+PiID).value += (data.message + '\n');
     };
 
     logSocket.onclose = function(e) {
         console.error('socket closed unexpectedly');
     };
 
-    // document.querySelector('#log-input'+PiID.toString()).focus();
-    document.querySelector('#log-input'+PiID.toString()).onkeyup = function(e) {
+    // document.querySelector('#log-input'+PiID).focus();
+    document.querySelector('#log-input'+PiID).onkeyup = function(e) {
         if (e.key === 'Enter') {  // enter, return
-            document.querySelector('#log-submit'+PiID.toString()).click();
+            document.querySelector('#log-submit'+PiID).click();
         }
     };
 
-    document.querySelector('#log-submit'+PiID.toString()).onclick = function(e) {
-        const messageInputDom = document.querySelector('#log-input'+PiID.toString());
-        const message = messageInputDom.value;
+    document.querySelector('#log-submit'+PiID).onclick = function(e) {
+        const messageInputDom = document.querySelector('#log-input'+PiID);
+        const message = 'send: ' + messageInputDom.value;
         logSocket.send(JSON.stringify({
             'message': message
         }));
         messageInputDom.value = '';
     };
-};
 
+    document.querySelector('#submit-reset'+PiID).onclick = function(e) {
+
+        e.preventDefault();
+
+        logSocket.send(
+            JSON.stringify({ 'message': 'reset: '+PiID })
+        );
+
+        fetch('/snmp/toggle', {
+          method: 'POST',
+            // name="port" value="2"
+            // { "port": PiID }
+          body: new FormData(document.querySelector('#form-reset2'))
+          // body: JSON.stringify({ port: [PiID,] }),
+          // headers: { "Content-type": "application/json; charset=UTF-8" }
+          }
+        )
+        .then((error) => console.log(error));
+    };
+
+};
