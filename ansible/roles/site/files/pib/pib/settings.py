d935bfd8ee66478cb36d9924b363139f61e16f13
Carl Karsten <carl@personnelware.com>
2024-09-20 20:07:53 -0500

    implement main pages as django templates

--- ansible/roles/site/files/pib/pib/settings.py
+++ ansible/roles/site/files/pib/pib/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pib',
+    'pibfpgas',
     'pibup',
     'snmp_switch',
     'pistat',
--- ansible/roles/site/files/pib/pib/urls.py
+++ ansible/roles/site/files/pib/pib/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 
+from django.urls import path
 from django.urls import include
 
-# import snmp_switch.urls
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-#     path('snmp/', include('snmp_switch.urls')),
-#    path('pistat/', include('pistat.urls')),
+    path('', view=RedirectView.as_view(url="/pibfpgas/")),
+    path('pibfpgas/', include('pibfpgas.urls')),
+    path('snmp/', include('snmp_switch.urls')),
+    path('pistat/', include('pistat.urls')),
     path('pibup/', include('pibup.urls')),
 ]
