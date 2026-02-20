de56c142d373d8997c84cc21572125da7dfc237d
Carl Karsten <carl@personnelware.com>
2026-02-20 14:25:07 -0600

    use by-id to find usb-MicroPython_Board

--- ansible/roles/site/files/pib/pibfpgas/Demos/kianv1/sit.sh
+++ ansible/roles/site/files/pib/pibfpgas/Demos/kianv1/sit.sh
@@ -3,4 +3,4 @@
 # sit.sh - send it
 # sends a file to the tt06 910 kianv1 via little_term.py
 
-cat ${1} | cstream -b 1 -t 30 > /dev/ttyACM0
+cat ${1} | cstream -b 1 -t 30 > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_de640cb1d3975a26-if00
